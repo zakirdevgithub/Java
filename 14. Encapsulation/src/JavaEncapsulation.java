1c9a73ea60cafb65b0d5c3aac9a469e132203c43
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 20:43:16 +0600

    Add files via upload

---
+++ 14. Encapsulation/src/JavaEncapsulation.java
@@ -0,0 +1,17 @@
+
+public class JavaEncapsulation {
+
+	public static void main(String[] args) {
+		
+		Person p= new Person();
+		p.setName("Zakir");
+		p.setAge(25);
+		p.setWieght(70.5);
+		System.out.println(p.getName());
+		System.out.println(p.getAge());
+		System.out.println(p.getWieght());
+		
+
+	}
+
+}
---
+++ 14. Encapsulation/src/Person.java
@@ -0,0 +1,27 @@
+
+public class Person {
+	private String name;
+	private int age;
+	private double wieght;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public double getWieght() {
+		return wieght;
+	}
+	public void setWieght(double wieght) {
+		this.wieght = wieght;
+	}
+	
+	
+}
