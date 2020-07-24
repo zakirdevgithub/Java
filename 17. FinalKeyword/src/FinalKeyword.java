797d0e9368dff077458e216da33fb5385d902152
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 20:44:40 +0600

    Add files via upload

---
+++ 17. FinalKeyword/src/FinalKeyword.java
@@ -0,0 +1,28 @@
+class FinalKey{
+	public final int add(int a, int b) {
+		return a+b;
+	}
+}
+
+class Add{
+	
+/*Override not possible because Parent class method declare with final
+ * 
+ * */
+//	@Override
+//	public final int add(int a, int b) {
+//		return a+b;
+//	}
+}
+
+
+public class FinalKeyword {
+
+	public static void main(String[] args) {
+		
+			final double pi=3.1416;
+//			pi=5.678;  // not possible because 
+			
+	}
+
+}
